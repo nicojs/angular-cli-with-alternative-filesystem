7ed7a7129fa7f5461fb6ddc4584cd4be0bbbf9ae
Nico Jansen <jansennico@gmail.com>
2018-01-04 21:18:10 +0100

    Add example with changing input file

--- main.js
+++ main.js
@@ -5,36 +5,46 @@ const AngularCompilerPlugin = require('@ngtools/webpack/src/angular_compiler_plu
 const ReplaceNodeOperation = require('@ngtools/webpack/src/transformers/interfaces').ReplaceNodeOperation;
 const makeTransform = require('@ngtools/webpack/src/transformers/make_transform').makeTransform;
 const ts = require('typescript');
+const fs = require('fs');
 
 const angularPlugin = webpackConfig.plugins.find(plugin => plugin instanceof AngularCompilerPlugin);
 
-const appComponent = path.resolve(__dirname, 'src', 'app', 'app.component.ts');
+const appComponentFileName = path.resolve(__dirname, 'src', 'app', 'app.component.ts');
+let appComponentContent = fs.readFileSync(appComponentFileName, 'utf8');
 
 function sourceFileTransformer(sourceFile) {
   const resolvedName = appComponent.replace(/\\/g, '/');
   if (sourceFile.fileName === resolvedName) {
-    return [new ReplaceNodeOperation(sourceFile, sourceFile, ts.createSourceFile(sourceFile.fileName, `alert('hello world!')`, ts.ScriptKind.TS))];
+    return [new ReplaceNodeOperation(sourceFile, sourceFile, ts.createSourceFile(sourceFile.fileName, appComponentContent, ts.ScriptKind.TS))];
   } else {
     return [];
   }
 }
 
-angularPlugin._transformers.unshift(makeTransform(sourceFileTransformer));
+// angularPlugin._transformers.unshift(makeTransform(sourceFileTransformer));
 
 const compiler = webpack(webpackConfig);
-const fs = compiler.inputFileSystem;
-const originalReadFile = fs.readFile;
-fs.readFile = function (name, optionalArgs, callback) {
-  if (name === appComponent) {
+const inputFS = compiler.inputFileSystem;
+const originalReadFile = inputFS.readFile;
+inputFS.readFile = function (name, optionalArgs, callback) {
+  if (name === appComponentFileName) {
     if (!callback) {
       callback = optionalArgs;
     }
-    callback(null, `alert('hello world!')`);
+    callback(null, appComponentContent);
   } else {
-    originalReadFile.apply(fs, [name, optionalArgs, callback]);
+    originalReadFile.apply(inputFS, [name, optionalArgs, callback]);
   }
 }
 
 compiler.run(() => {
-  console.log('Done');
+  console.log('Done ... 1');
+
+  setTimeout(() => {
+    appComponentContent = 'alert("hello world")';
+    compiler.run(() => {
+      console.log('Done ... 2');
+    });
+  }, 1000);
+
 });
